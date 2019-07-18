61e8cd19e53dafb873b973962a78d0731384eae7
cladams <cladams0203@gmail.com>
2019-07-18 14:24:29 -0400

    the begining

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+
+
+function App () {
+  const [keys, steKeys] = useState();
+
+  return (
+    <div>
+      <h1>Drum Kit in React</h1>
+    </div>
+  )
+}
+
+export default App;
